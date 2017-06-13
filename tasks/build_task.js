328b95f49780b7fb16024c7cf0cad33a8c199bc0
James Yin <coocoon@gmail.com>
2017-06-13 12:45:12 +0800

    Warning: Task "karma:test" failed. Use --force to continue.

--- tasks/build_task.js
+++ tasks/build_task.js
@@ -3,6 +3,8 @@ var path = require('path');
 module.exports = function(grunt) {
   "use strict";
 
+  grunt.option("force");
+
   // Concat and Minify the src directory into dist
   grunt.registerTask('build', [
     'jshint:source',
